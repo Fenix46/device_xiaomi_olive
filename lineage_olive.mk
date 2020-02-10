9109370532fd74259dd8fec3070d528fee09105a
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-11 01:31:11 +0530

    olive-user-9-PKQ1.190319.001-V11.0.1.0.PCNRUXM-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_olive.mk
+++ lineage_olive.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Redmi 8
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := olive
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="olive-user 9 PKQ1.190319.001 V11.0.1.0.PCNINXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="olive-user 9 PKQ1.190319.001 V11.0.1.0.PCNRUXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/olive/olive:9/PKQ1.190319.001/V11.0.1.0.PCNINXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/olive_ru/olive:9/PKQ1.190319.001/V11.0.1.0.PCNRUXM:user/release-keys
